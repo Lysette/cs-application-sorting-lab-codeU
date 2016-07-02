0c66f5ac5a6d6061aa2a7b2a34b5366fa927e32a
Lysette <isjochan@gmail.com>
2016-07-01 23:08:20 -0400

    completed lab

--- javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
+++ javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
@@ -5,6 +5,7 @@ package com.flatironschool.javacs;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
@@ -63,8 +64,55 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public List<T> mergeSort(List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
-        return null;
+        // TODO
+		
+		// 0. if one item or less in list, return that list
+		if (list.size() <= 1) {
+			return list;
+		}
+		
+		// 1. split list in half
+		int i = list.size() / 2;
+		
+		List<T> half1 = list.subList(0, i);
+		List<T> half2 = list.subList(i, list.size());
+		
+		// 2. sort each half
+		half1 = mergeSort(half1, comparator);
+		half2 = mergeSort(half2, comparator);
+
+		List<T> newList = new ArrayList<T>();
+		int i1 = 0;
+		int i2 = 0;
+		
+		// 3. merge both halves into newList
+		// exit when go thru all items in one half
+		while (i1 < half1.size() && i2 < half2.size()) {
+			int c = comparator.compare(half1.get(i1), half2.get(i2));
+			
+			// item in half1 is smaller or equal to item in half2
+			if (c <= 0) {
+				newList.add(half1.get(i1));
+				i1++;
+			}
+			else {
+				newList.add(half2.get(i2));
+				i2++;
+			}
+		}
+		
+		// add rest of list, exit when go through both halves
+		while (i1 < half1.size() || i2 < half2.size()) {
+			if (i1 < half1.size()) {
+				newList.add(half1.get(i1));
+				i1++;
+			}
+			else {
+				newList.add(half2.get(i2));
+				i2++;
+			}
+		}
+        return newList;
 	}
 
 	/**
@@ -75,9 +123,18 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public void heapSort(List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
+        // TODO
+		
+		PriorityQueue<T> queue = new PriorityQueue<T>();
+		
+		for (T item : list) {
+			queue.offer(item);
+		}
+		
+		for (int i = 0; i < list.size(); i++) {
+			list.set(i, queue.poll());
+		}
 	}
-
 	
 	/**
 	 * Returns the largest `k` elements in `list` in ascending order.
@@ -89,8 +146,29 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public List<T> topK(int k, List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
-        return null;
+        // TODO
+		
+		PriorityQueue<T> queue = new PriorityQueue<T>();
+		
+		for (T item : list) {
+			// add if queue not full
+			if (queue.size() < k) {
+				queue.add(item);
+			}
+			// add if item is bigger than smallest item in queue
+			else if (comparator.compare(item, queue.peek()) > 0){
+				queue.remove();
+				queue.add(item);
+			}
+		}
+		
+		List<T> newList = new ArrayList<T>();
+		
+		while (!queue.isEmpty()) {
+			newList.add(queue.poll());
+		}
+		
+        return newList;
 	}
 
 	
